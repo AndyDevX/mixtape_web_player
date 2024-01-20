ee185fec9df78866b983e1697bbf51b14d37fdfc
AndyDevX <andresag253@gmail.com>
2024-01-19 20:29:56 -0600

    Inclusión de mixtapes a partir de un JSON

--- public/script.js
+++ public/script.js
@@ -1,9 +1,33 @@
-var aVariable = 1;
+fetch('public/mixtapes.json')
+  .then(response => response.json())
+  .then(data => {
+    const playlists = data.mixtapes;
+    const contenedorPlaylists = document.getElementById('mixtapes');
 
-for (var i = 0; i <= 10; i++) {
-    console.log(aVariable);
-    let x = 4;
-}
+    // Iterar sobre cada playlist y crear la estructura HTML
+    playlists.forEach(playlist => {
+      // Crear elementos HTML
+      const mixtape = document.createElement('div');
+      mixtape.className = 'mixtape';
 
-console.log(x);
+      const mixtapeImage = document.createElement('div');
+      mixtapeImage.className = 'mixtape_image';
 
+      const imagen = document.createElement('img');
+      imagen.src = playlist.cover; // Puedes personalizar la imagen según tus necesidades
+      imagen.alt = playlist.autor;
+
+      const mixtapeTitle = document.createElement('span');
+      mixtapeTitle.className = 'mixtape-title';
+      mixtapeTitle.textContent = playlist.name;
+
+      // Adjuntar elementos al árbol DOM
+      mixtapeImage.appendChild(imagen);
+      mixtape.appendChild(mixtapeImage);
+      mixtape.appendChild(mixtapeTitle);
+
+      // Agregar la nueva mixtape al contenedor principal
+      contenedorPlaylists.appendChild(mixtape);
+    });
+  })
+  .catch(error => console.error('Error al leer el archivo JSON:', error));
